997ee4971aa30c042695c4382055bb660c0c3795
martinez2720 <105889165+martinez2720@users.noreply.github.com>
2023-10-21 17:42:23 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,11 @@
+a=33
+b="33"
+
+if a>b:
+    print("b é maior que a")
+elif b==a:
+    print("a e b são iguais")
+
+
+
+
---
+++ teste2.py
@@ -0,0 +1,10 @@
+
+def my_function (fname): 
+    print (fname + " Refsnes")
+
+my_function("Emilly")
+
+my_function("Tobias")
+
+my_function("Lucas")
+    
